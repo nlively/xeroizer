96bf23584cf5c5f72cacdee20129f07f3c6cdc3c
Noah Lively <noah@koremedia.net>
2015-05-14 18:18:55 -0700

    make user agent optional in xeroizer lib

--- lib/xeroizer/http.rb
+++ lib/xeroizer/http.rb
@@ -16,8 +16,8 @@ module Xeroizer
   module Http
 
     ACCEPT_MIME_MAP = {
-      :pdf  => 'application/pdf',
-      :json => 'application/json'
+        :pdf => 'application/pdf',
+        :json => 'application/json'
     }
 
     # Shortcut method for #http_request with `method` = :get.
@@ -51,146 +51,162 @@ module Xeroizer
 
     private
 
-      def http_request(client, method, url, body, params = {})
-        # headers = {'Accept-Encoding' => 'gzip, deflate'}
+    def http_request(client, method, url, body, params = {})
+      # headers = {'Accept-Encoding' => 'gzip, deflate'}
 
-        headers = { 'charset' => 'utf-8' }
+      headers = {'charset' => 'utf-8'}
 
-        if method != :get
-          headers['Content-Type'] ||= "application/x-www-form-urlencoded"
-        end
+      if method != :get
+        headers['Content-Type'] ||= "application/x-www-form-urlencoded"
+      end
 
-        content_type = params.delete(:content_type)
-        headers['Content-Type'] = content_type if content_type
+      if params[:user_agent].present?
+        headers['User-Agent'] = params[:user_agent]
+      end
 
-        # HAX.  Xero completely misuse the If-Modified-Since HTTP header.
-        headers['If-Modified-Since'] = params.delete(:ModifiedAfter).utc.strftime("%Y-%m-%dT%H:%M:%S") if params[:ModifiedAfter]
+      content_type = params.delete(:content_type)
+      headers['Content-Type'] = content_type if content_type
 
-        # Allow 'Accept' header to be specified with :accept parameter.
-        # Valid values are :pdf or :json.
-        if params[:response]
-          response_type = params.delete(:response)
-          headers['Accept'] = case response_type
-            when Symbol then  ACCEPT_MIME_MAP[response_type]
-            else              response_type
-          end
-        else
-          headers['Accept'] = "application/xml"
-        end
+      # HAX.  Xero completely misuse the If-Modified-Since HTTP header.
+      headers['If-Modified-Since'] = params.delete(:ModifiedAfter).utc.strftime("%Y-%m-%dT%H:%M:%S") if params[:ModifiedAfter]
 
-        if params.any?
-          url += "?" + params.map {|key,value| "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"}.join("&")
-        end
+      # Allow 'Accept' header to be specified with :accept parameter.
+      # Valid values are :pdf or :json.
+      if params[:response]
+        response_type = params.delete(:response)
+        headers['Accept'] = case response_type
+                              when Symbol then
+                                ACCEPT_MIME_MAP[response_type]
+                              else
+                                response_type
+                            end
+      else
+        headers['Accept'] = "application/xml"
+      end
 
-        uri   = URI.parse(url)
+      if params.any?
+        url += "?" + params.map { |key, value| "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}" }.join("&")
+      end
 
-        attempts = 0
+      uri = URI.parse(url)
 
-        begin
-          attempts += 1
-          logger.info("XeroGateway Request: #{method.to_s.upcase} #{uri.request_uri}") if self.logger
+      attempts = 0
 
-          raw_body = params.delete(:raw_body) ? body : {:xml => body}
+      begin
+        attempts += 1
+        logger.info("XeroGateway Request: #{method.to_s.upcase} #{uri.request_uri}") if self.logger
 
-          response = case method
-            when :get   then    client.get(uri.request_uri, headers)
-            when :post  then    client.post(uri.request_uri, raw_body, headers)
-            when :put   then    client.put(uri.request_uri, raw_body, headers)
-          end
+        raw_body = params.delete(:raw_body) ? body : {:xml => body}
 
-          if self.logger
-            logger.info("XeroGateway Response (#{response.code})")
-            unless response.code.to_i == 200
-              logger.info("== #{uri.request_uri} Response Body \n\n #{response.plain_body} \n == End Response Body")
-            else
-              logger.debug("== #{uri.request_uri} Response Body \n\n #{response.plain_body} \n == End Response Body")
-            end
-          end
+        response = case method
+                     when :get then
+                       client.get(uri.request_uri, headers)
+                     when :post then
+                       client.post(uri.request_uri, raw_body, headers)
+                     when :put then
+                       client.put(uri.request_uri, raw_body, headers)
+                   end
 
-          case response.code.to_i
-            when 200
-              response.plain_body
-            when 400
-              handle_error!(response, body)
-            when 401
-              handle_oauth_error!(response)
-            when 404
-              handle_object_not_found!(response, url)
-            when 503
-              handle_oauth_error!(response)
-            else
-              raise "Unknown response code: #{response.code.to_i}"
-          end
-        rescue Xeroizer::OAuth::RateLimitExceeded
-          if self.rate_limit_sleep
-            raise if attempts > rate_limit_max_attempts
-            logger.info("Rate limit exceeded, retrying") if self.logger
-            sleep_for(self.rate_limit_sleep)
-            retry
+        if self.logger
+          logger.info("XeroGateway Response (#{response.code})")
+          unless response.code.to_i == 200
+            logger.info("== #{uri.request_uri} Response Body \n\n #{response.plain_body} \n == End Response Body")
           else
-            raise
+            logger.debug("== #{uri.request_uri} Response Body \n\n #{response.plain_body} \n == End Response Body")
           end
         end
-      end
 
-      def handle_oauth_error!(response)
-        if response.plain_body == "You do not have permission to access this resource." || response.plain_body.match(/API access not authorised/i)
-          raise LackingPermissionToAccessRecord.new(response)
+        case response.code.to_i
+          when 200
+            response.plain_body
+          when 400
+            handle_error!(response, body)
+          when 401
+            handle_oauth_error!(response)
+          when 404
+            handle_object_not_found!(response, url)
+          when 503
+            handle_oauth_error!(response)
+          else
+            raise "Unknown response code: #{response.code.to_i}"
         end
-        
-        error_details = CGI.parse(response.plain_body)
-        description   = error_details["oauth_problem_advice"].first
-
-        # see http://oauth.pbworks.com/ProblemReporting
-        # In addition to token_expired and token_rejected, Xero also returns
-        # 'rate limit exceeded' when more than 60 requests have been made in
-        # a second.
-        case (error_details["oauth_problem"].first)
-          when "token_expired"        then raise OAuth::TokenExpired.new(description)
-          when "token_rejected"       then raise OAuth::TokenInvalid.new(description)
-          when "rate limit exceeded"  then raise OAuth::RateLimitExceeded.new(description)
-          else raise OAuth::UnknownError.new(error_details["oauth_problem"].first + ':' + description)
-        end
-      end
-
-      def handle_error!(response, request_body)
-
-        raw_response = response.plain_body
-
-        # XeroGenericApplication API Exceptions *claim* to be UTF-16 encoded, but fail REXML/Iconv parsing...
-        # So let's ignore that :)
-        raw_response.gsub! '<?xml version="1.0" encoding="utf-16"?>', ''
-
-        # doc = REXML::Document.new(raw_response, :ignore_whitespace_nodes => :all)
-        doc = Nokogiri::XML(raw_response)
-
-        if doc && doc.root && doc.root.name == "ApiException"
-
-          raise ApiException.new(doc.root.xpath("Type").text,
-                                 doc.root.xpath("Message").text,
-                                 raw_response,
-                                 doc,
-                                 request_body)
-
+      rescue Xeroizer::OAuth::RateLimitExceeded
+        if self.rate_limit_sleep
+          raise if attempts > rate_limit_max_attempts
+          logger.info("Rate limit exceeded, retrying") if self.logger
+          sleep_for(self.rate_limit_sleep)
+          retry
         else
-
-          raise "Unparseable 400 Response: #{raw_response}"
-
-        end
-
-      end
-
-      def handle_object_not_found!(response, request_url)
-        case(request_url)
-          when /Invoices/ then raise InvoiceNotFoundError.new("Invoice not found in Xero.")
-          when /CreditNotes/ then raise CreditNoteNotFoundError.new("Credit Note not found in Xero.")
-          else raise ObjectNotFound.new(request_url)
+          raise
         end
       end
+    end
 
-      def sleep_for(seconds = 1)
-        sleep seconds
+    def handle_oauth_error!(response)
+      if response.plain_body == "You do not have permission to access this resource." || response.plain_body.match(/API access not authorised/i)
+        raise LackingPermissionToAccessRecord.new(response)
       end
+
+      error_details = CGI.parse(response.plain_body)
+      description = error_details["oauth_problem_advice"].first
+
+      # see http://oauth.pbworks.com/ProblemReporting
+      # In addition to token_expired and token_rejected, Xero also returns
+      # 'rate limit exceeded' when more than 60 requests have been made in
+      # a second.
+      case (error_details["oauth_problem"].first)
+        when "token_expired" then
+          raise OAuth::TokenExpired.new(description)
+        when "token_rejected" then
+          raise OAuth::TokenInvalid.new(description)
+        when "rate limit exceeded" then
+          raise OAuth::RateLimitExceeded.new(description)
+        else
+          raise OAuth::UnknownError.new(error_details["oauth_problem"].first + ':' + description)
+      end
+    end
+
+    def handle_error!(response, request_body)
+
+      raw_response = response.plain_body
+
+      # XeroGenericApplication API Exceptions *claim* to be UTF-16 encoded, but fail REXML/Iconv parsing...
+      # So let's ignore that :)
+      raw_response.gsub! '<?xml version="1.0" encoding="utf-16"?>', ''
+
+      # doc = REXML::Document.new(raw_response, :ignore_whitespace_nodes => :all)
+      doc = Nokogiri::XML(raw_response)
+
+      if doc && doc.root && doc.root.name == "ApiException"
+
+        raise ApiException.new(doc.root.xpath("Type").text,
+                               doc.root.xpath("Message").text,
+                               raw_response,
+                               doc,
+                               request_body)
+
+      else
+
+        raise "Unparseable 400 Response: #{raw_response}"
+
+      end
+
+    end
+
+    def handle_object_not_found!(response, request_url)
+      case (request_url)
+        when /Invoices/ then
+          raise InvoiceNotFoundError.new("Invoice not found in Xero.")
+        when /CreditNotes/ then
+          raise CreditNoteNotFoundError.new("Credit Note not found in Xero.")
+        else
+          raise ObjectNotFound.new(request_url)
+      end
+    end
+
+    def sleep_for(seconds = 1)
+      sleep seconds
+    end
 
   end
 end
